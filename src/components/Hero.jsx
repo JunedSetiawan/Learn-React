58e54f0967439bc2d1727816c24f2e2630cfbf9d
JunedSetiawan <junedhayko502@gmail.com>
2023-05-30 10:05:20 +0700

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,10 @@ function Hero() {
 
     return(
           <div className="max-w-md">
-        <h1 className="text-5xl font-bold">Halo Halo</h1>
-        <img className="mx-auto mt-4 motion-safe:animate-[spin_3s_linear_infinite]" src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg" alt="" loading='lazy' />
-        <p className="py-4">Proses Belajar React JS
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed itaque consequatur eos suscipit placeat inventore magni sapiente qui numquam.
+        <h1 className="text-5xl font-bold">Halo</h1>
+        <img className="mx-auto mt-4 mb-3 motion-safe:animate-[spin_3s_linear_infinite]" src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg" alt="" loading='lazy' />
+          Disini saya belajar <span className="text-primary font-bold font-mono">React JS</span>
+        <p className="py-4">Proses Belajar <span className="font-bold">35%</span>
         </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
